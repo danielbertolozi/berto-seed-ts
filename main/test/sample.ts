8a73b30f311646babfc92b25d174a658f942bdc2
Daniel Bertolozi <daniel.bertolozi@sap.com>
2019-10-01 19:36:05 -0300

    Initial commit

---
+++ main/test/sample.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+
+context("A first test", () => {
+	describe("when running mocha", () => {
+		it("should run without problems", () => {
+			expect(true).to.be.true;	
+		});	
+	});	
+});
